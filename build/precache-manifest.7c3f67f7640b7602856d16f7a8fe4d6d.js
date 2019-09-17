8b807a2917dd648f1b323e817ed906a5b2870c34
anavicente <anacoelhovicente@gmail.com>
2019-09-17 10:48:40 +0100

    initial commit

---
+++ build/precache-manifest.7c3f67f7640b7602856d16f7a8fe4d6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1293282d42b1fe031a6",
+    "url": "/static/js/main.b1293282.chunk.js"
+  },
+  {
+    "revision": "5cb1329abe115dde4a4d",
+    "url": "/static/js/1.5cb1329a.chunk.js"
+  },
+  {
+    "revision": "b1293282d42b1fe031a6",
+    "url": "/static/css/main.a44126c5.chunk.css"
+  },
+  {
+    "revision": "5cb1329abe115dde4a4d",
+    "url": "/static/css/1.836a5762.chunk.css"
+  },
+  {
+    "revision": "b60bcd110121d937b98cca2c5489eaf8",
+    "url": "/index.html"
+  }
+];
